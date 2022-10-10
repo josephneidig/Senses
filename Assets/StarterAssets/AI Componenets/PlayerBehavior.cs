5742c06fb3b0044ebd9745da561740828d4bcd59
Kyle Mauter <kylemauter@knights.ucf.edu>
2022-10-09 22:39:35 -0400

    Player takes damage from collision with enemies.

---
+++ Assets/StarterAssets/AI Componenets/PlayerBehavior.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    public float dmgTimer;
+    public float dmgTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayerTakeDmg(int dmg)
+    {
+        GameManager.gameManager._playerHealth.DmgUnit(dmg);
+        Debug.Log(GameManager.gameManager._playerHealth.Health);
+    }
+
+    private void PlayerHeal(int healing)
+    {
+        GameManager.gameManager._playerHealth.HealUnit(healing);
+        Debug.Log(GameManager.gameManager._playerHealth.Health);
+    }
+
+    // I ADDED THESE
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Enemy"))
+        {
+            PlayerTakeDmg(10);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Enemy"))
+        {
+            if (dmgTimer < dmgTrigger)
+            {
+                dmgTimer += Time.deltaTime;
+            }
+            else
+            {
+                PlayerTakeDmg(10);
+                dmgTimer = 0;
+            }
+        }
+    }
+}
