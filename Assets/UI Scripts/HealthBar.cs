400376daadc4d5e57800baafc6fad958b38f662f
Ma1e <cadejackson2003@gmail.com>
2022-10-14 13:10:19 -0400

    Health Bar/Death Screen

--- Assets/UI Scripts/HealthBar.cs
+++ Assets/UI Scripts/HealthBar.cs
@@ -7,17 +7,30 @@ public class HealthBar : MonoBehaviour
 {
     public Slider healthBar;
     public Health playerHealth;
+	public GameObject DeathScreen;
 
     private void Start()
     {
+		Hide();
         playerHealth = GameObject.FindGameObjectWithTag("Player").GetComponent<Health>();
         healthBar = GetComponent<Slider>();
         healthBar.maxValue = playerHealth.maxHealth;
         healthBar.value = playerHealth.maxHealth;
     }
+	
+	public void Died ()
+	{
+		DeathScreen.SetActive(true);
+	}
+	public void Hide()
+	{
+		DeathScreen.SetActive(false);
+	}
 
     public void SetHealth(int hp)
     {
         healthBar.value = hp;
+		if(hp <= 0)
+			Died();
     }
 }
