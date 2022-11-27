ce458ef00dc2167d52edce3d797133a76873f437
jenniennie <90437083+jenniennie@users.noreply.github.com>
2022-11-27 01:23:56 -0500

    doors!

---
+++ Assets/doorscript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doorscript : MonoBehaviour
+{
+    Animator animator;
+    bool doorOpen;
+
+    void Start()
+    {
+        Debug.Log("starting");
+
+
+        doorOpen = false;
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+
+            doorOpen = true;
+            Doors("open");
+        }
+    }
+
+     void OnTriggerExit(Collider col)
+    {
+        if(doorOpen)
+        {
+            doorOpen = false;
+            Doors("close");
+        }
+    }
+
+    void Doors(string direction)
+    {
+        animator.SetTrigger(direction);
+    }
+}
